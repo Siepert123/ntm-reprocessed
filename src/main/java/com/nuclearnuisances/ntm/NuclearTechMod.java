b2fbda6beae082402b82cb67810a7fc689f0edff
yor <yoonch1022@naver.com>
2024-12-03 23:58:04 +0900

    add model registry

--- src/main/java/com/nuclearnuisances/ntm/NuclearTechMod.java
+++ src/main/java/com/nuclearnuisances/ntm/NuclearTechMod.java
@@ -4,11 +4,14 @@ import com.nuclearnuisances.ntm.block.NTMBlocks;
 import com.nuclearnuisances.ntm.item.NTMItems;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SuppressWarnings("unused")
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
@@ -22,6 +25,7 @@ public class NuclearTechMod {
     /* ----- REGISTRY EVENTS ----- */
     @SubscribeEvent public void registerItems(RegistryEvent.Register<Item> event) { NTMItems.registerItems(event); }
     @SubscribeEvent public void registerBlocks(RegistryEvent.Register<Block> event) { NTMBlocks.registerBlocks(event); }
+    @SideOnly(Side.CLIENT) @SubscribeEvent public void registerModels(ModelRegistryEvent event) { NTMItems.registerModel(event); }
 
     /* ----- EVENTS ----- */
 }
